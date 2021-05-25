5f4c1345d0d81187fd7694009d8569241bae3d31
theom95 <theodore.mitchell95@gmail.com>
2021-05-25 14:54:46 -0500

    Got the calculator display to work

--- main.js
+++ main.js
@@ -42,4 +42,31 @@ var operation = {
     operator: '/'  
 };
 
+function populateScreen(value) {
+    if (value === '+/-') {
+        calculatorDisplayText.innerHTML *= -1;
+    } else if (displayValue === '0') {
+        calculatorDisplayText.innerHTML = value;
+    } else {
+        calculatorDisplayText.innerHTML += value;
+    }
+    displayValue = calculatorDisplayText.innerHTML;
+}
+
+function processButtonInput(event){
+    let pressedButton = event.target;
+
+    if (pressedButton.classList.contains("number")) {
+        populateScreen(pressedButton.innerHTML);
+    } else if (pressedButton.classList.contains("operator")) {
+
+    }
+}
+
+const calculatorDisplayText = document.querySelector('#calculatorDisplayText');
+let displayValue = calculatorDisplayText.innerHTML;
+
+const numberButtons = document.querySelectorAll('.button');
+numberButtons.forEach(button => button.addEventListener('click', processButtonInput));
+
 // console.log(operate(operation));
