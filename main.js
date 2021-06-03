00ba8cf2b3c40fd7aa3ba3fde33e45db8a52fc2e
theom95 <theodore.mitchell95@gmail.com>
2021-06-02 20:08:23 -0500

    Calculator logic should be complete now

--- main.js
+++ main.js
@@ -11,6 +11,9 @@ function multiply (number1, number2) {
 }
 
 function divide (number1, number2) {
+    if (number2 == 0){
+        return "lmao"
+    } 
 	return number1 / number2;
 }
 
@@ -22,6 +25,7 @@ function erase (operation){
 
 function operate (operation) {
     let result = null;
+    console.log(operation)
     switch(operation.operator) {
         case '+':
             result = add(operation.firstNumber, operation.secondNumber);
@@ -29,23 +33,22 @@ function operate (operation) {
         case '-':
             result = subtract(operation.firstNumber, operation.secondNumber);
             break;
-        case '*':
+        case '×':
             result = multiply(operation.firstNumber, operation.secondNumber);
             break;
-        case '/':
+        case '÷':
             result = divide(operation.firstNumber, operation.secondNumber);
             break;
         default:
             result = 'Incorrect request, try again';
     } 
-    // console.log("this is result!!");
-    // console.log(result);
     return result;
 }
 
-function processNumber(value) {
-    if (value === '+/-') {
-        calculatorDisplayText.innerHTML *= -1;
+function processNumber(operation, value) {
+    if (operation.operator === '=') {
+        calculatorDisplayText.innerHTML = value;
+        operation.operator = undefined;
     } else if (currentNumberValue === '0') {
         calculatorDisplayText.innerHTML = value;
     } else {
@@ -55,31 +58,68 @@ function processNumber(value) {
 }
 
 function processOperator(operation, operatorValue){
-    if (operatorValue === 'AC') {
-        erase(operation);
-        calculatorDisplayText.innerHTML = '0';
-    } else if (operation.firstNumber === undefined){
-        operation.firstNumber = parseInt(currentNumberValue);
-        operation.operator = operatorValue;        
-    } else {
-        operation.secondNumber = parseInt(currentNumberValue);
-        calculationResult = operate(operation);
-        calculatorDisplayText.innerHTML = calculationResult;
-        operation.firstNumber = calculationResult;
-        operation.secondNumber = undefined;
+    currentNumberValue = calculatorDisplayText.innerHTML;
+
+    switch(operatorValue) {
+        case 'AC':
+            erase(operation);
+            calculatorDisplayText.innerHTML = '0';
+            currentNumberValue = '0';
+            break;
+        case '=':
+            if (operation.firstNumber === undefined || operation.operator === undefined){
+                break;
+            }
+        default:
+            if (operation.firstNumber === undefined){
+                operation.firstNumber = parseFloat(currentNumberValue);    
+            } else {
+                operation.secondNumber = parseFloat(currentNumberValue);
+                calculationResult = operate(operation);
+                calculatorDisplayText.innerHTML = calculationResult;
+                operation.firstNumber = calculationResult;
+                operation.secondNumber = undefined;
+    
+                if (operatorValue === '=') {
+                    erase(operation);
+                }
+            }
+            operation.operator = operatorValue;
+            currentNumberValue = '0'; 
     }
-    currentNumberValue = '0';
+    console.log(operation)
 }
 
 function processButtonInput(event){
     let pressedButton = event.target;
 
     if (pressedButton.classList.contains("number")) {
-        processNumber(pressedButton.innerHTML);
+        processNumber(operation, pressedButton.innerHTML);
     } else if (pressedButton.classList.contains("operator")) {
         processOperator(operation, pressedButton.innerHTML);
+    } else if (pressedButton.classList.contains("sign")) {
+        calculatorDisplayText.innerHTML *= -1;
     }
 }
+
+// function removeTransition(e) {
+//     console.log("i am remove transition");
+//     if (e.propertyName !== 'transform') return;
+//     e.target.classList.remove('playing');
+//     console.log("i actually ran");
+// }
+
+// function clickButton(e) {
+//     // const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+//     const key = document.querySelector(`button[data-key="${e.keyCode}"]`);
+//     console.log(e);
+//     console.log(key);
+
+//     if (!key) return;
+//     key.classList.add('clickedButton');
+//     // audio.currentTime = 0;
+//     // audio.play();
+// }
 
 const calculatorDisplayText = document.querySelector('#calculatorDisplayText');
 let currentNumberValue = calculatorDisplayText.innerHTML;
@@ -90,5 +130,7 @@ var operation = {
     operator: undefined  
 };
 
-const numberButtons = document.querySelectorAll('.button');
-numberButtons.forEach(button => button.addEventListener('click', processButtonInput));
+const calculatorButtons = document.querySelectorAll('.button');
+calculatorButtons.forEach(button => button.addEventListener('click', processButtonInput));
+// calculatorButtons.forEach(button => button.addEventListener('transitionend', removeTransition));
+// window.addEventListener('keydown', clickButton);
